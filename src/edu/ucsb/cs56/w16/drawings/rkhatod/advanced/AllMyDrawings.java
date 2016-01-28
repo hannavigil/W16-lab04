2362bd6cf5c133c18c6ce2ca18585f0540fa3a51
Radhika Khatod <rkhatod@umail.ucsb.edu>
2016-01-27 17:42:17 -0800

    lab done

--- src/edu/ucsb/cs56/w16/drawings/rkhatod/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/w16/drawings/rkhatod/advanced/AllMyDrawings.java
@@ -28,19 +28,8 @@ public class AllMyDrawings
 
 	Players p1 = new Players(400, 150, 50, 50);
 	g2.setColor(Color.BLACK); g2.draw(p1);
-	/**	
-	// Make a black house that's half the size, 
-	// and moved over 150 pixels in x direction
 	
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
-	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	
+	Players p2 = new Players (300, 150, 50, 50);
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
 	
@@ -51,22 +40,8 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
-	
-	// Draw two houses with Windows
-	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
-	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
-	
-	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
-	
-	g2.setStroke(orig);
-	g2.setColor(Color.BLACK); 
-	g2.drawString("A few houses by Phill Conrad", 20,20);
-	*/
+	g2.draw(p2); 
+       
     }
     
     
@@ -81,20 +56,15 @@ public class AllMyDrawings
 	Players p1 = new Players(400, 150, 50, 50);
 	g2.setColor(Color.PINK); g2.draw(p1);
 
-       	Players p2 = new Players(300, 150, 50, 50);
+	//Make a Magenta player thats double the size of p1 
+	//and moved over 150 pixels to the left in the x direction
+	Shape p2 = ShapeTransforms.scaledCopyOfLL(p1,2.0,2.0);
+	p2 = ShapeTransforms.translatedCopyOf(p2,150,0);
 	g2.setColor(Color.MAGENTA); g2.draw(p2);
-	/**
-	// Make a black house that's half the size, 
-	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+
+	Players p3 = new Players(50, 50, 100, 100);
+	g2.setColor(Color.BLUE); g2.draw(p3);
 	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	*/
     }
     public static void drawPicture3(Graphics2D g2) {
 	
@@ -103,13 +73,20 @@ public class AllMyDrawings
 	g2.drawString("A bunch of basketball courts and players by Radhika Khatod", 20,20);
 	
 	
-	// Draw some coffee cups.
+	// Draw some courts.
 	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
+	Players large = new Players(200,10,150,225);
+	Players smallBC = new Players(20,50,50,75);
+
 	
 	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
+	// We'll draw this with a thicker stroke
+	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
+	Stroke orig=g2.getStroke();
+	g2.setStroke(thick);
+	g2.setColor(new Color(0x7B68EE)); 
+	g2.draw(smallBC); 
+       	
 	
     }       
 }
