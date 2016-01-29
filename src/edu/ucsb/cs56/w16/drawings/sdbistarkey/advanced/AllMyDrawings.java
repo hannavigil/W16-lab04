1f32b00a64ecf202e64d52c4ead5f3a78151ed77
Skyler Bistarkey-Rez <sdb@umail.ucsb.edu>
2016-01-28 16:16:29 -0800

    Now have customized myMPV and myMPV2 fully. TODO: customized myMPV3

--- src/edu/ucsb/cs56/w16/drawings/sdbistarkey/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/w16/drawings/sdbistarkey/advanced/AllMyDrawings.java
@@ -67,62 +67,32 @@ public class AllMyDrawings
 	// Draw some coffee cups.
 	
 	SmartPhoneWithBackground sp1 =
-	    new SmartPhoneWithBackground(100, 400, 100, 200);
+	    new SmartPhoneWithBackground(100, 300, 50, 100);
         Shape sp2 =
-	    ShapeTransforms.translatedCopyOf(sp1, 0, -200);
+	    ShapeTransforms.translatedCopyOf(sp1, 0, -100);
 	Shape sp3 =
 	    ShapeTransforms.rotatedCopyOf(sp2, Math.PI/2.0);
-	sp3 = ShapeTransforms.translatedCopyOf(sp3, 0, -150);
+	sp3 = ShapeTransforms.translatedCopyOf(sp3, 75, 50);
+
+	Shape sp4 =
+	    ShapeTransforms.translatedCopyOf(sp1, 150, 0);
+	Shape sp5 =
+	    ShapeTransforms.translatedCopyOf(sp2, 150, 0);
+	Shape sp6 =
+	    ShapeTransforms.scaledCopyOf(sp1, 1.0, 2.0);
+	sp6 = ShapeTransforms.translatedCopyOf(sp6, 300, -100);
 	
 	g2.setColor(Color.RED);     g2.draw(sp1);
 	g2.setColor(Color.GREEN);   g2.draw(sp2);
 	g2.setColor(Color.BLUE);    g2.draw(sp3);
-	g2.setColor(Color.MAGENTA);
-	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
-	
-	// Make a black house that's half the size, 
-	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
-	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	
-	// We'll draw this with a thicker stroke
-	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
-	
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
-	
-	Stroke orig=g2.getStroke();
-	g2.setStroke(thick);
-	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
-	
-	// Draw two houses with Windows
-	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
-	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); 
-	
-	// Rotate the second house 45 degrees around its center.
-	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
-	
-	g2.draw(hw3);
+	g2.setColor(Color.MAGENTA); g2.draw(sp4);
+	g2.setColor(Color.BLACK);   g2.draw(sp5);
+	g2.setColor(Color.PINK);    g2.draw(sp6);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
-	
-	g2.setStroke(orig);
+        
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
+	g2.drawString("Smart Phones in the shape of \"Hi\" by Skyler Bistarkey-Rez", 20,20);
     }
     
     /** Draw a different picture with a few houses and coffee cups
