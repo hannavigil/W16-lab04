76d03e78fdec4d45e51acae5c63deb59086819c3
Christopher Beser <cjbeser@umail.ucsb.edu>
2016-01-28 21:10:51 -0800

    ChocolateChipCookie subclass completed

---
+++ src/edu/ucsb/cs56/w16/drawings/beserchris/advanced/ChocolateChipCookie.java
@@ -0,0 +1,57 @@
+package edu.ucsb.cs56.w16.drawings.beserchris.advanced;
+import java.awt.geom.GeneralPath;  
+import java.awt.Shape; 
+import java.awt.geom.Ellipse2D;
+
+/**
+   A Chocolate Chip Cookie
+      
+   @author Christopher Beser
+   @version for CS56, W16, UCSB
+   
+*/
+public class ChocolateChipCookie extends Cookie implements Shape
+{
+    /**
+     * Constructor for objects of class ChocolateChipCookie
+     */
+    public ChocolateChipCookie(double x, double y, double radius)
+    {
+	//construct the Cookiel
+	super(x,y,radius);
+	
+	// get the GeneralPath that we are going to append stuff to
+	GeneralPath gp = this.get();
+	
+	
+	double chipOffset = radius/2;
+	Ellipse2D.Double [] chips = {
+	    new Ellipse2D.Double(x + chipOffset, y + chipOffset, 2, 2),
+
+	    new Ellipse2D.Double(x + chipOffset-radius/4, y + chipOffset-radius/4, 2, 2),
+
+	    new Ellipse2D.Double(x + chipOffset + radius/4, y + chipOffset - radius/4, 2, 2),
+
+	    new Ellipse2D.Double(x + chipOffset + radius/4, y + chipOffset + radius/4, 2, 2),
+
+	    new Ellipse2D.Double(x + chipOffset - radius/4, y + chipOffset + radius/4, 2, 2),
+
+	    new Ellipse2D.Double(x + chipOffset , y + chipOffset - radius/3, 2, 2),
+
+	    new Ellipse2D.Double(x + chipOffset , y + chipOffset + radius/3, 2, 2), 
+
+	    new Ellipse2D.Double(x + chipOffset - radius/3 , y + chipOffset , 2, 2),
+
+	    new Ellipse2D.Double(x + chipOffset + radius/3 , y + chipOffset , 2, 2)
+
+	    
+	};
+	//add the chips to the cookie
+	
+        GeneralPath theCookie  = this.get();
+	for(Ellipse2D.Double chip : chips){
+	    theCookie.append(chip,false);
+	}
+
+    }    
+}
--- src/edu/ucsb/cs56/w16/drawings/beserchris/advanced/Cookie.java
+++ src/edu/ucsb/cs56/w16/drawings/beserchris/advanced/Cookie.java
@@ -19,8 +19,8 @@ public class Cookie  extends GeneralPathWrapper implements Shape
     /**
        Constructor
        
-       @param x x coord of lower left corner of house
-       @param y y coord of lower left corner of house
+       @param x x coord of lower left corner of Cookie
+       @param y y coord of lower left corner of Cookie
        @param Radius of the Cookie
     */
     public Cookie(double x, double y, double radius)
@@ -32,27 +32,6 @@ public class Cookie  extends GeneralPathWrapper implements Shape
 	GeneralPath theCookie = this.get();
 	theCookie.append(outerCookie, false);
 	theCookie.append(innerCookie, false);
-        // Rectangle2D.Double firstStory = 
-        //     new Rectangle2D.Double(x, firstStoryUpperLeftY ,
-	// 			   width, firstStoryHeight);
-	
-        // // make the roof.   Remember that y goes DOWN the page,
-        // // so we ADD to y to get a "lower" value on the screen
-        
-        // Line2D.Double leftRoof = 
-        //     new Line2D.Double (x, y + roofHeight,
-        //                        x + width/2.0, y);
-	
-        // Line2D.Double rightRoof =
-        //     new Line2D.Double (x + width/2.0, y,
-        //                        x + width, y + roofHeight);
-	
-        // // put the whole house together
-	
-        // GeneralPath wholeHouse = this.get();
-        // wholeHouse.append(firstStory, false);
-        // wholeHouse.append(leftRoof, false);
-        // wholeHouse.append(rightRoof, false); 
-	
+       
     }
 }
