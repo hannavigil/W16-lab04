ae353c61a456451ef73a42125471f36986cef273
Tristan <ttstarck@umail.ucsb.edu>
2016-02-08 14:58:13 -0800

    Animated Drawing Working

--- src/edu/ucsb/cs56/w16/drawings/ttstarck/advanced/Clock.java
+++ src/edu/ucsb/cs56/w16/drawings/ttstarck/advanced/Clock.java
@@ -60,4 +60,88 @@ public class Clock extends GeneralPathWrapper implements Shape
         wholeClock.append(bigHand, false);
         wholeClock.append(smallHand, false);    
     }
+    public Clock(double x, double y, double radius, int minute, int hour)
+    {
+
+        Ellipse2D.Double circle =
+            new Ellipse2D.Double(x, y,
+				 radius, radius);
+	double minuteHand = (double)minute;
+	double hourHand = (double)hour;
+	boolean minuteXDown = false;
+	boolean minuteYDown = false;
+	boolean hourXDown = false;
+	boolean hourYDown = false;
+	if(minute <= 15){
+	    minuteHand = minuteHand/15.0;
+	    minuteYDown = true;
+	}
+	else if(minute >15 && minute <= 30){
+	    minuteHand = minute-15.0;
+	    minuteHand = minuteHand / 15.0;
+	    minuteHand = 1-minuteHand;
+	}
+	else if(minute > 30 && minute <= 45){
+	    minuteHand = minute-30.0;
+	    minuteHand = minuteHand/15.0;
+	    minuteXDown = true;
+	}
+	else if (minute > 45 && minute < 60){
+	    minuteHand = minute-45.0;
+	    minuteHand = minuteHand/15.0;
+	    minuteHand = 1-minuteHand;
+	    minuteXDown = true;
+	    minuteYDown = true;
+	}
+
+	double bigHandX = minuteHand * radius;
+        if(minuteXDown == true)
+            bigHandX = bigHandX*-1;
+        double bigHandY = Math.sqrt(Math.pow(radius,2)-Math.pow(bigHandX,2));
+        if( minuteYDown)
+            bigHandY = bigHandY*-1;
+
+        Line2D.Double bigHand =
+            new Line2D.Double (x+radius/2,y+radius/2,
+                               x +radius/2+ bigHandX/2, y+radius/2+bigHandY/2);
+	if(hour<= 3){
+	    hourHand = hourHand/3.0;
+	    hourYDown = true;
+	}
+	else if(hour >3 && hour <= 6){
+	    hourHand = hour-3.0;
+	    hourHand = hourHand/3.0;
+	    hourHand = 1-hourHand;
+	}
+	else if(hour > 6 && hour <= 9){
+	    hourHand = hour-6.0;
+	    hourHand = hourHand/3.0;
+	    hourXDown = true;
+	}
+	else{
+	    hourHand = hour-9.0;
+	    hourHand = hourHand/3.0;
+	    hourHand = 1-hourHand;
+	    hourXDown = true;
+	    hourYDown = true;
+	}
+
+	double smallHandX = hourHand * radius;
+        if(hourXDown)
+            smallHandX = smallHandX*-1;
+        double smallHandY = Math.sqrt(Math.pow(radius,2)-Math.pow(smallHandX,2));
+        if(hourYDown)
+            smallHandY = smallHandY*-1;
+        Line2D.Double smallHand =
+            new Line2D.Double (x+radius/2, y+radius/2, x + radius/2 +smallHandX/4, y +radius/2+ smallHandY/4);
+
+        // put the whole clock together                                               
+  
+
+        GeneralPath wholeClock = this.get();
+        wholeClock.append(circle, false);
+        wholeClock.append(bigHand, false);
+        wholeClock.append(smallHand, false);
+    }
+
 }
